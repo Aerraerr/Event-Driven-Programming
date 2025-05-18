f10854ebbdc36b4ba0fb8218d337bda526d02387
Aerraerr <marquezaeronjead@gmail.com>
2025-05-18 12:00:59 +0800

    Initial commit to Event-Driven-Programming

---
+++ EDP_Activity7/EDP_Activity7/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace EDP_Activity7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox1.Image = EDPwithGUI.Properties.Resources.Grey_and_White_Retro_Fitness_and_Gym_Emblem_Logo__1__removebg_preview;
+            pictureBox1.Location = new Point(2, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(582, 661);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click_2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DimGray;
+            button1.FlatAppearance.BorderColor = Color.Purple;
+            button1.FlatStyle = FlatStyle.System;
+            button1.Font = new Font("Impact", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.AppWorkspace;
+            button1.Location = new Point(375, 531);
+            button1.Name = "button1";
+            button1.Size = new Size(175, 43);
+            button1.TabIndex = 1;
+            button1.Text = "LOGIN";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DimGray;
+            button2.FlatAppearance.MouseDownBackColor = Color.FromArgb(255, 128, 255);
+            button2.FlatStyle = FlatStyle.System;
+            button2.Font = new Font("Impact", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.AppWorkspace;
+            button2.Location = new Point(375, 580);
+            button2.Name = "button2";
+            button2.Size = new Size(175, 43);
+            button2.TabIndex = 2;
+            button2.Text = "REGISTER";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightGray;
+            BackgroundImage = EDPwithGUI.Properties.Resources.gymrat_removebg_preview;
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(582, 653);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            DoubleBuffered = true;
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Palong-Palo Gym";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ EDP_Gym/packages/Siticone.NetFramework.UI.2025.4.14/Readme.md
@@ -0,0 +1,18 @@
+﻿
+__Welcome to Siticone NetFrameworkUI Library__
+
+__We have released new controls and now we have 100+ UI elements__
+
+Siticone NetFrameworkUI is a modern UI library for .NET Framework 4.8 and above. Build fast, sleek, and high-performance WinForms desktop apps with hardware-accelerated, pixel-perfect controls—seamlessly integrated for a polished, modern experience.
+
+_Happy UI/UX Development!_ 😊
+
+---
+
+__IMPORTANT.__
+
+If you have any questions, do not hesitate to contact us anytime.
+
+Thank you.
+
+https://www.siticoneframework.com/
